b09cca93d3d8823c1ea5926d3ba7525cd220e10c
Adam Mizera <amizera@gmail.com>
2023-01-02 07:01:39 +0100

    space correction

--- src/Pages/Lacalizations/Lacalizations.jsx
+++ src/Pages/Lacalizations/Lacalizations.jsx
@@ -63,8 +63,6 @@ export default function Lacalizations() {
         ...prevLocation,
         [key]: [...prevLocation.images, value]
       }))
-
-      
       console.log("index true: " + index)
     }
 
